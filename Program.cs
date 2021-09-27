7a1d744da1a1fed00a33af2209f4f3f79aa4ca81
Loi <loivp13@gmail.com>
2021-09-27 14:37:13 -0700

    thread pools

--- Program.cs
+++ Program.cs
@@ -11,48 +11,43 @@ namespace Threading
     {
         static void Main(string[] args)
         {
-           /* Console.WriteLine("Hello World 1");
-            Thread.Sleep(1000);
-            Console.WriteLine("Hello World 2");
-            Console.WriteLine("Hello World 3");
-            Console.WriteLine("Hello World 4");*/
+            /* Console.WriteLine("Hello World 1");
+             Thread.Sleep(1000);
+             Console.WriteLine("Hello World 2");
+             Console.WriteLine("Hello World 3");
+             Console.WriteLine("Hello World 4");*/
 
-  /*          new Thread(() =>
+            /*          new Thread(() =>
+                      {
+                          Thread.Sleep(1000);
+                          Console.WriteLine("Thread 1");
+                      }).Start();
+
+                       new Thread(() =>
+                      {
+                          Thread.Sleep(1000);
+                          Console.WriteLine("Thread 2");
+                      }).Start();
+
+                       new Thread(() =>
+                      {
+                          Thread.Sleep(1000);
+                          Console.WriteLine("Thread 3");
+                      }).Start();
+
+                       new Thread(() =>
+                      {
+                          Thread.Sleep(1000);
+                          Console.WriteLine("Thread 4");
+                      }).Start();*/
+
+            Enumerable.Range(0, 100).ToList().ForEach(f =>
             {
+                Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
+
                 Thread.Sleep(1000);
-                Console.WriteLine("Thread 1");
-            }).Start();
-
-             new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 2");
-            }).Start();
-
-             new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 3");
-            }).Start();
-
-             new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 4");
-            }).Start();*/
-
-
-            var taskCompletionSource = new TaskCompletionSource<bool>();
-
-            var thread = new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                taskCompletionSource.TrySetResult(true);
             });
-            Console.WriteLine($"Thread number: {thread.ManagedThreadId}");
-            thread.Start();
-            var test = taskCompletionSource.Task.Result;
-            Console.ReadLine();
+                Console.ReadLine();
         }
     }
 }
